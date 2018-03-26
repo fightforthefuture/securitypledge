97344d9c7ac02de7b1f2cffed4203630e49d65ec
Ken Mickles <ken@37i.net>
2018-03-26 15:29:00 -0400

    [WIP] browser test + cleanup

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
-/**
- *
- * @source: https://github.com/fightforthefuture
- *
- * @licstart  The following is the entire license notice for the
- *  JavaScript code in this page.
- *
- * Copyright (C) Fight for the Future
- *
- *
- * The JavaScript code in this page is free software: you can
- * redistribute it and/or modify it under the terms of the GNU
- * General Public License (GNU GPL) as published by the Free Software
- * Foundation, either version 3 of the License, or (at your option)
- * any later version.  The code is distributed WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS
- * FOR A PARTICULAR PURPOSE.  See the GNU GPL for more details.
- *
- * As additional permission under GNU GPL version 3 section 7, you
- * may distribute non-source (e.g., minimized or compacted) forms of
- * that code without the copy of the GNU GPL normally required by
- * section 4, provided you include this license notice and a URL
- * through which recipients can access the Corresponding Source.
- *
- * @licend  The above is the entire license notice
- * for the JavaScript code in this page.
- *
- */
+document.addEventListener("DOMContentLoaded", function(event) {
+  var app = new Vue({
+    el: '#app',
+
+    data: function() {
+      return {
+        name: null,
+        email: null,
+        zipCode: null,
+        modalVisible: false
+      }
+    },
+
+    methods: {
+      submitFFTFForm: function() {
+        var self = this;
+        self.modalVisible = true;
+      },
+
+      submitDPForm: function() {
+
+      },
+
+      shareOnTwitter: function() {
+        console.log('share on twitter')
+      },
+
+      shareOnFacebook: function() {
+        console.log('share on facebook')
+      }
+    }
+  });
+
+  return
 
 // modals.js
 (function (doc, win) {
@@ -438,3 +444,5 @@
     });
   }
 })(document, window);
+
+});
